ed128e64aec7daf5da04ad85ac57769ea3d15a5d
Sari Shehada <sarishehada@gmail.com>
2023-10-27 19:02:11 +0300

    Changed time zone and added static-root

--- graduationProject/settings.py
+++ graduationProject/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Riyadh'
 
 USE_I18N = True
 
@@ -113,7 +113,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = (os.path.join(BASE_DIR , 'static'))
 STATIC_URL = '/static/'
 MEDIA_ROOT ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
