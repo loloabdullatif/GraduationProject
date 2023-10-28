8f62176df77140b5cd6e939827a61e8ee39693f6
Sari Shehada <sarishehada@gmail.com>
2023-10-28 13:46:14 +0300

    Fixed a bug where account creation doesnt return id with the user object

--- api/views.py
+++ api/views.py
@@ -35,8 +35,8 @@ def getById(request):
 def createAccount(request):
     serializer= AddUserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
+        user= serializer.save()
+        return Response(UserReturnSerializer(user).data,status=status.HTTP_201_CREATED)
     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 # @api_view(['PUT'])
