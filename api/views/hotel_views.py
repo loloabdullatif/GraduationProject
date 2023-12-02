1c2a999f667f59e64c9fc22b781e8d658ce3abc5
Sari Shehada <sarishehada@gmail.com>
2023-12-02 18:47:29 +0300

    Trending hotels end-point now uses HotelDetailsSerializer too

--- api/views/hotel_views.py
+++ api/views/hotel_views.py
@@ -21,75 +21,7 @@ def hotels(request):
 
 def getHotels(request):
     hotels = Hotel.objects.all()
-    hotelsWithDetails = []
-    for hotel in hotels:
-        hotelsWithDetails.append(getHotelDetails(hotel, request))
-    return Response(hotelsWithDetails, status=status.HTTP_200_OK)
-
-
-def getHotelDetails(hotel, request):
-    services = Service.objects.filter(publicPlaceId=hotel.id)
-    hotelAmenities = []
-    for service in services:
-        hotelAmenities.append(service.amenityId)
-    # hotelAmenities = Amenities.objects.filter(id__in=amenitiesIds)
-    hotelImages = Images.objects.filter(publicPlaceId=hotel.id)
-
-    return {
-        'hotel': HotelResponseSerializer(hotel).data,
-        'amenities': AmenitySerializer(hotelAmenities, many=True).data,
-        'images': ImageSerializer(hotelImages, context={'request': request}, many=True).data,
-        'location': PublicPlaceFullAddressSerializer(hotel).data
-    }
-
-
-# @api_view(['POST'])
-# def addHotel(request):
-#     data = request.data
-#     print('Add Hotel Request Received')
-#     print('Hotel Data Provided:' + getString(request.data))
-#     hotelDict = data.get('hotel')
-#     if hotelDict == None:
-#         return Response(data='No hotel was provided', status=status.HTTP_400_BAD_REQUEST)
-#     if isinstance(hotelDict, str):
-#         hotelDict = json.loads(hotelDict)
-#     hotelDict['type'] = 'hotel'
-#     amenities = data.get('amenities')
-#     if amenities == None:
-#         return Response(data='No amenities were provided', status=status.HTTP_400_BAD_REQUEST)
-#     if isinstance(amenities, str):
-#         amenities = json.loads(amenities)
-#         amenities = [int(i) for i in amenities]
-#     images = request.FILES
-#     hotelSerializer = AddHotelSerializer(data=hotelDict)
-#     if hotelSerializer.is_valid():
-#         hotel = hotelSerializer.save()
-#         publicPlaceId = hotel.id
-
-#         if isinstance(amenities, Iterable):
-#             for amenityId in amenities:
-#                 serviceSerializer = ServiceSerializer(data={
-#                     'publicPlaceId': publicPlaceId,
-#                     'amenityId': amenityId,
-#                 })
-#                 if serviceSerializer.is_valid():
-#                     serviceSerializer.save()
-#                 else:
-#                     return Response(serviceSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#         if isinstance(images, Iterable):
-#             for imageKey in images.keys():
-#                 imageSerializer = ImageSerializer(data={
-#                     'publicPlaceId': publicPlaceId,
-#                     'path': images[imageKey],
-#                 })
-#                 if imageSerializer.is_valid():
-#                     imageSerializer.save()
-#                 else:
-#                     return Response(imageSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#         print('Add Hotel Request Was Handled Successfully')
-#         return Response(True, status=status.HTTP_201_CREATED)
-#     return Response(hotelSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(HotelDetailsSerializer(hotels, context={'request': request}, many=True).data, status=status.HTTP_200_OK)
 
 
 def getString(param):
@@ -108,7 +40,6 @@ class AddHotelAPIView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(True)
-        print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
